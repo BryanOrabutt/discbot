4900c80e73fd240de6c8368180882c65bb64d6f5
Bryan Orabutt <bryan.orabutt@gmail.com>
2017-02-18 16:30:54 -0600

    Working test build

--- bots/Bastion.py
+++ bots/Bastion.py
@@ -9,30 +9,27 @@ bot_name = "Bastion"
 client = discord.Client()
 logger = create_logger(bot_name)
 bot_data = create_filegen(bot_name)
+wrclient = wolframalpha.Client('L247V6-5JHVJQVEYE')
 
 help_msg = '''
 Commands available:\n!wrq <query> - Queries Wolfram Alpha with the provided input query
 '''
 
 @register_command
-async def WolframQuery(msg, mobj):
-    try:
-        with open(join(KEY_FOLDER, "{}.key".format(bot_name)), 'r') as f:
-            q = f.read().strip("\n").strip("\r").replace("\n", "")
-            f.close()
-    except:
-        raise IOError("Can't read key")
-
-    client = wolframalpha.Client(q)
-    res = client.query(str(msg))
+async def wrq(msg, mobj):
+    res = wrclient.query(str(msg))
+    results = ''
     for pod in res.pods:
         for sub in pod.subpods:
              imgs = re.findall(urlmarker.URL_REGEX, str(sub))
-             results = ''
              for s in imgs:
-                 results = results + s + '\n'
-    return await client.send_message(mobj.channel, pre_text(results))
+                 results = results + s + ' '
+    return await client.send_message(mobj.channel, results)
 
 @register_command
 async def howto(msg, mobj):
     return await client.send_message(mobj.channel, pre_text(help_msg))
+
+setup_all_events(client, bot_name, logger)
+if __name__ == "__main__":
+    run_the_bot(client, bot_name, logger)
