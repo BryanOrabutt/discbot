eb812316bc6195cba746d7f2f7290e1981ae4c0b
BryanOrabutt <bryan.orabutt@gmail.com>
2018-04-15 19:40:47 -0500

    test

--- bots/Bastion.py
+++ bots/Bastion.py
@@ -257,7 +257,7 @@ async def cat(msg, mobj):
     """
 
     if msg == '':
-        img,imgid = catapi.getCat()
+        img,imgid = catapi.getCat(imgid=None,category=None)
         #returnmsg = img + '\nimage id = ' + imgid
         return await client.send_message(mobj.channel, "Test")
     else:
